b622551b3cfa6f5fe284686e1159df75524d934b
Grazzini Ezio <grazzini2@mmi-dev.unilim.fr>
2026-04-22 16:50:30 +0200

    ajout template movie

---
+++ app/component/Movie/script.js
@@ -0,0 +1,38 @@
+let templateFile = await fetch("./component/Movie/template.html");
+let template = await templateFile.text();
+
+let templateFile2 = await fetch("./component/Movie/templateLi.html");
+let template2 = await templateFile2.text();
+
+let MovieList = {};
+
+// MovieList.formatOneMenu = function(name, img){
+//     let li = template2;
+//     console.log(name)
+//     li = li.replace("{{name}}", name);
+//     li = li.replace("{{img}}", img);
+//     return li;
+// }
+
+MovieList.format = function (data) {
+  let html = template;
+
+  let liste="";
+
+  for (let item of data){
+    let li = template2;
+    li = li.replace("{{name}}", item.name);
+    li = li.replace("{{img}}", item.image);
+
+    liste = liste += li;
+  }
+  html = html.replace("{{movies}}", liste)
+  return html;
+};
+
+MovieList.render = function(where, what){
+    let element = document.querySelector(where)
+    element.innerHTML += MovieList.format(what);
+}
+
+export { MovieList };
