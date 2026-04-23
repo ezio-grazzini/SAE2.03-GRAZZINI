e929f6139b79e0e10d729a4b311fd8ce1dae7974
Grazzini Ezio <grazzini2@mmi-dev.unilim.fr>
2026-04-23 11:57:12 +0200

    itération 2

---
+++ admin/component/MovieForm/script.js
@@ -0,0 +1,35 @@
+let templateFile = await fetch("./component/Movie/template.html");
+let template = await templateFile.text();
+
+let MovieList = {};
+
+// MovieList.formatOneMenu = function(name, img){
+//     let li = template2;
+//     console.log(name)
+//     li = li.replace("{{name}}", name);
+//     li = li.replace("{{img}}", img);
+//     return li;
+// }
+
+MovieList.format = function (data) {
+  let html = template;
+
+  let liste="";
+
+  for (let item of data){
+    let li = template2;
+    li = li.replace("{{name}}", item.name);
+    li = li.replace("{{img}}", "../server/images/" + item.image);
+
+    liste = liste += li;
+  }
+  html = html.replace("{{movies}}", liste)
+  return html;
+};
+
+MovieList.render = function(where, what){
+    let element = document.querySelector(where)
+    element.innerHTML += MovieList.format(what);
+}
+
+export { MovieList };
